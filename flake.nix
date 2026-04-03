2c927dc6fdfd2b1b11296d8f7f0f40c2b7ba9955
Clayton Rush Williams <Clayton.R.Williams@protonmail.com>
2026-04-03 09:43:04 -0400

    feat: switch to upstream ggml-org/llama.cpp

--- flake.nix
+++ flake.nix
@@ -1,36 +1,15 @@
 {
-  description = "Hardware-Accelerated llama.cpp Perk Card";
+  description = "Hardware-Accelerated llama.cpp (Upstream)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    llama-cpp.url = "github:ggml-org/llama.cpp";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, llama-cpp }:
     let
       system = "x86_64-linux";
-      # We create a version of nixpkgs that allows unfree (for CUDA)
-      # and explicitly uses CUDA support
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          cudaSupport = true;
-        };
-      };
     in {
-      packages.${system}.default = (pkgs.llama-cpp.override {
-        # This tells Nix to use the CUDA 12 toolkit for the build
-        cudaPackages = pkgs.cudaPackages_12;
-      }).overrideAttrs (old: {
-        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
-          "-DCMAKE_CUDA_ARCHITECTURES=120"
-        ];
-      });
-
-      # Allow running it directly
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/llama-cli";
-      };
+      packages.${system}.default = llama-cpp.packages.${system}.cuda;
     };
 }
